be7b3f024a9f79e25388f6ac6b852237895a813a
fantasticsoul <zhongzhengkai@gmail.com>
2022-09-02 06:15:40 +0800

    feat:re deploy

--- subApp.js
+++ subApp.js
@@ -3,6 +3,6 @@ const pkg = require('./package.json');
 
 // const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg' });
 
-const subApp = helDevUtils.createVue2SubApp(pkg, { defaultHomePage: 'https://hel-eco.github.io/hel-tpl-remote-vue-comp/_v1' });
+const subApp = helDevUtils.createVue2SubApp(pkg, { defaultHomePage: 'https://hel-eco.github.io/hel-tpl-remote-vue-comp/as_v1' });
 
 module.exports = subApp;
