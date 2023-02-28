06ebdc9bd9b9bcd00789faf6f3f7729e7eae25ef
fantasticsoul <zhongzhengkai@gmail.com>
2023-02-28 09:16:41 +0800

    feat: update subApp

--- subApp.js
+++ subApp.js
@@ -1,13 +1,12 @@
 const helDevUtils = require('hel-dev-utils');
-const pkg = require('./package.json');
+const pkg = require('../package.json');
 
-// deploy to unpkg
 const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg' });
 
-// deploy to self deployed unpkg
-// const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg', homePage: 'http://my-unpkg:8888' });
+// 自定义 homePage，形如：https://youhost.com/aa/bb、 /aa/bb、../aa/bb
+// const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg', homePage: './xx' });
 
-// deploy to git
-// const subApp = helDevUtils.createVue2SubApp(pkg, { homePage: 'https://hel-eco.github.io/hel-tpl-remote-vue-comp/as_v1' });
+// 自定义 homePage，不拼接名字和版本号
+// const subApp = helDevUtils.createVue2SubApp(pkg, { npmCdnType: 'unpkg', homePage: './xx', handleHomePage: false });
 
 module.exports = subApp;
