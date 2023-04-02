668dcb5495f47432e0bdecd9dbb38ce91d7eb550
fantasticsoul <zhongzhengkai@gmail.com>
2023-04-02 21:29:52 +0800

    feat: bump hel to v4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
+import { isMasterApp } from 'hel-iso';
 import * as Vue from 'vue';
 import { LIB_NAME } from './configs/subApp';
-import { libReady, isSubApp } from 'hel-lib-proxy';
+import { libReady } from 'hel-lib-proxy';
 // import { preFetchLib } from 'hel-micro';
 
 if (Vue.config) {
@@ -18,7 +19,7 @@ if (Vue.config) {
   // 非子应用时（即不是被别的模块触发载入的情况），自己挂载渲染节点，方便本地调试
   // 可根据项目实际情况控制是否载入 loadApp 文件
   // eslint-disable-next-line
-  if (!isSubApp()) {
+  if (isMasterApp()) {
     await import('./loadApp');
   }
-}()).catch(console.error);
+})().catch(console.error);
