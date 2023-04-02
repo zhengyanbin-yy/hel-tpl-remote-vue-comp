5e1dc57aa94d8a63f1106963b3ef020cc1313902
fantasticsoul <zhongzhengkai@gmail.com>
2023-04-02 21:26:42 +0800

    feat: bump hel to v4

--- scripts/meta.js
+++ scripts/meta.js
@@ -9,10 +9,10 @@ const process = require('process');
 const path = require('path');
 const helDevUtils = require('hel-dev-utils');
 const packageJson = require('../package.json');
-const subApp = require('../subApp');
+const appInfo = require('../appInfo');
 
 helDevUtils.extractHelMetaJson({
-  subApp,
+  appInfo,
   buildDirFullPath: path.join(__dirname, '../hel_dist'),
   packageJson,
 }).catch(err => {
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,17 @@
 const webpack = require('webpack');
 const helDevUtils = require('hel-dev-utils');
-const subApp = require('./subApp');
+const appInfo = require('./appInfo');
 // const webpack = require('webpack');
 
 module.exports = {
-  // 此处传入的url值仅为了方便另一个项目可以基于当前模块的wed-dev-server调试当前模块代码，端口号对齐 npm run start 里的 PORT
-  // 它不会影响流水线的 publicUrl 值，因为 hel-dev-utils 内部发现设置有 process.env.HEL_APP_HOME_PAGE 时或 设置了 npmCdnType 时，
-  // 会优先采用 HEL_APP_HOME_PAGE 值或 npmCdnType 对应的 cdn 前缀值作为 publicUrl，覆盖掉这里的默认值
-  publicPath: subApp.getPublicPathOrUrl('http://localhost:7001'),
+  publicPath: appInfo.getPublicPathOrUrl(),
   productionSourceMap: true,
   outputDir: helDevUtils.cst.HEL_DIST_DIR,
   configureWebpack: config => {
-    config.output.library = subApp.groupName;
+    config.output.library = appInfo.groupName;
     config.output.libraryTarget = 'umd';
-    config.output.jsonpFunction = subApp.jsonpFnName;
-    config.externals = subApp.externals;
+    config.output.jsonpFunction = appInfo.jsonpFnName;
+    config.externals = appInfo.externals;
     config.optimization = {
       splitChunks: {
         maxSize: 6000000,
